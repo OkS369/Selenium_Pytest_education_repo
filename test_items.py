340afd7c973d9a4b6af586d9fa4693f3c975a29f
OkS369 <matviivrm@gmail.com>
2021-06-28 18:21:50 +0300

    First upload

---
+++ test_items.py
@@ -0,0 +1,11 @@
+import pytest
+from selenium import webdriver
+import time
+
+
+def test_link(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    browser.get(link)
+    time.sleep(30)
+    button = browser.find_elements_by_css_selector('#add_to_basket_form > button')
+    assert len(button) == 1, 'Test failed. More than 1 button or button not found'
