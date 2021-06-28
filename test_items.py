1245e28c05e681bcd2cd14971f5eb09e8b146d40
OkS369 <matviivrm@gmail.com>
2021-06-28 18:39:34 +0300

    Turned off delay (commented row with "time.sleep(30)")

--- test_items.py
+++ test_items.py
@@ -6,6 +6,6 @@ import time
 def test_add_to_basket_button_at_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
     browser.get(link)
-    time.sleep(30)
+    #time.sleep(30)
     button = browser.find_elements_by_css_selector('#add_to_basket_form > button')
     assert len(button) == 1, 'Test failed. More than 1 button or button not found'
